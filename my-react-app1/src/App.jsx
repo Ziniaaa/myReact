940b1f70d738bd88f5ece0edf64f0f76a3a66b26
Ziniaaa <ziniania0727@gmail.com>
2024-12-02 11:50:00 +0800

    1202+

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,14 +1,17 @@
 import { useEffect } from "react"
 import axios from 'axios';
 import './App.css'
+import { IoIosUmbrella } from "react-icons/io";
 
 
 function App() {
 
   useEffect(() => {
     (async () => {
-      const data = await axios.get('./F-C0032-001.json');
+      // const data = await axios.get('./F-C0032-001.json');
+      const data = await axios.get('https://github.com/Ziniaaa/myReact/blob/main/my-react-app1/src/F-C0032-001.json');
       const { location } = data.data.cwaopendata.dataset;
+      const options = { hour: 'numeric', minute: 'numeric' };
       console.log(location);
 
     })
@@ -20,66 +23,67 @@ function App() {
 
   return (
     <>
-      <h2>36小時天氣預報</h2>
 
-      <section>
-        <div className="city">
-          <h3>臺北市</h3>
-          <div className="date">
-            <div className="period">
-            <p>2日</p>
-            <p>上午6:00<br />~<br />下午6:00</p>
-            <p><img src="./weatherIcon/晴時多雲.svg" alt="" /></p>
-            <p>晴時多雨</p>
-            <p><span>☂</span>10%</p>
+      <section className="wrap">
+        <h2>36小時天氣預報</h2>
+        <div className="city2">
+          <div className="city">
+            <h3>臺北市</h3>
+            <div className="date">
+              <div className="period">
+                <p className="pDate">2日</p>
+                <p>上午6:00<br />~<br />下午6:00</p>
+                <p><img src="./weatherIcon/晴時多雲.svg" alt="" /></p>
+                <p>晴時多雨</p>
+                <p className="pRain"><IoIosUmbrella />10%</p>
+              </div>
+
+              <div className="period">
+                <p className="pDate">2日</p>
+                <p>下午6:00<br />~<br />上午6:00</p>
+                <p><img src="./weatherIcon/晴時多雲.svg" alt="" /></p>
+                <p>晴時多雨</p>
+                <p className="pRain"><IoIosUmbrella />10%</p>
+              </div>
+
+              <div className="period">
+                <p className="pDate">3日</p>
+                <p>上午6:00<br />~<br />下午6:00</p>
+                <p><img src="./weatherIcon/晴時多雲.svg" alt="" /></p>
+                <p>晴時多雨</p>
+                <p className="pRain"><IoIosUmbrella />10%</p>
+              </div>
             </div>
-          
-                <div className="period">
-            <p>2日</p>
-            <p>下午6:00<br />~<br />上午6:00</p>
-            <p><img src="./weatherIcon/晴時多雲.svg" alt="" /></p>
-            <p>晴時多雨</p>
-            <p><span>icon</span>10%</p>
-            </div>
-          
-          <div className="period">
-            <p>3日</p>
-            <p>上午6:00<br />~<br />下午6:00</p>
-            <p><img src="./weatherIcon/晴時多雲.svg" alt="" /></p>
-            <p>晴時多雨</p>
-            <p><span>icon</span>10%</p>
+
+          </div>
+          <div className="city">
+            <h3>新北市</h3>
+            <div className="date">
+              <div className="period">
+                <p className="pDate">2日</p>
+                <p>上午6:00<br />~<br />下午6:00</p>
+                <p><img src="./weatherIcon/晴時多雲.svg" alt="" /></p>
+                <p>晴時多雨</p>
+                <p className="pRain"><IoIosUmbrella />10%</p>
+              </div>
+
+              <div className="period">
+                <p className="pDate">2日</p>
+                <p>下午6:00<br />~<br />上午6:00</p>
+                <p><img src="./weatherIcon/晴時多雲.svg" alt="" /></p>
+                <p>晴時多雨</p>
+                <p className="pRain"><IoIosUmbrella />10%</p>
+              </div>
+
+              <div className="period">
+                <p className="pDate">3日</p>
+                <p>上午6:00<br />~<br />下午6:00</p>
+                <p><img src="./weatherIcon/晴時多雲.svg" alt="" /></p>
+                <p>晴時多雨</p>
+                <p className="pRain"><IoIosUmbrella />10%</p>
+              </div>
             </div>
           </div>
-          
-        </div>
-        <div className="city">
-          <h3>新北市</h3>
-          <div className="date">
-            <div className="period">
-            <p>2日</p>
-            <p>上午6:00<br />~<br />下午6:00</p>
-            <p><img src="./weatherIcon/晴時多雲.svg" alt="" /></p>
-            <p>晴時多雨</p>
-            <p><span>icon</span>10%</p>
-            </div>
-          
-                <div className="period">
-            <p>2日</p>
-            <p>下午6:00<br />~<br />上午6:00</p>
-            <p><img src="./weatherIcon/晴時多雲.svg" alt="" /></p>
-            <p>晴時多雨</p>
-            <p><span>icon</span>10%</p>
-            </div>
-          
-          <div className="period">
-            <p>3日</p>
-            <p>上午6:00<br />~<br />下午6:00</p>
-            <p><img src="./weatherIcon/晴時多雲.svg" alt="" /></p>
-            <p>晴時多雨</p>
-            <p><span>icon</span>10%</p>
-            </div>
-          </div>
-          
         </div>
 
       </section>
