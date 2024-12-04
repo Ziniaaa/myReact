25c0234f2ac81232cfeac9ad70cecbec8dd0edc2
Ziniaaa <ziniania0727@gmail.com>
2024-12-04 10:55:29 +0800

    1204

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,11 +1,13 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from 'axios';
 import './App.css'
 import { IoIosUmbrella } from "react-icons/io";
-import ShowCity from "./ShowCity";
+
 
 
 function App() {
+  const [weatherList, setWeatherList] = useState([]);
+
 
   useEffect(() => {
     (async () => {
@@ -16,28 +18,70 @@ function App() {
       const data = await axios.get('https://ziniaaa.github.io/myReact/json/F-C0032-001.json');
       // console.log(data);
       const { location } = data.data.cwaopendata.dataset;
-      // console.log(location);
+      console.log(location);
+      setWeatherList(location);
 
-      // locatuinName → 縣市名稱 
-      // elementName → Wx → 天氣概況
-      // elementName → PoP → 下雨機率
-
-      function ShowCity(){
-      location.map((city) => {
-        return <div key={city.locationName}>{city.locationName}</div>
-      })}
 
     })()
   }, [])
 
 
   return (
+    // locatuinName → 縣市名稱 
+    // elementName → Wx → 天氣概況
+    // elementName → PoP → 下雨機率
     <>
       <section className="wrap">
         <h2>36小時天氣預報</h2>
-        <ShowCity/>
+        <div className="container">
+          {
+            weatherList.map((city) => {
+              return (
+                <div className="item" key={city.locationName}>
+                  <h3>{city.locationName}</h3>
+                  <div className="date">
+                    {
+                      city.weatherElement[0].time.map((time, index) => {
+                        return (
+                          <div className="period" key={index}>
+                            {/* 日期*/}
+                            <p className="pDate">
+                              {
+                                new Date(time.startTime).toLocaleString(undefined, { day: 'numeric' })
+                              }</p>
+                            {/* 時間 */}
+                            <p>
+                              {
+                                new Date(time.startTime).toLocaleString(undefined, { hour: 'numeric', minute: 'numeric' })
+                              }
+                              <br />~<br />
+                              {
+                                new Date(time.endTime).toLocaleString(undefined, { hour: 'numeric', minute: 'numeric' })
+                              }
+                            </p>
+                            {/* 天氣圖 */}
+                            <p><img src={`./weatherIcon/${time.parameter.parameterName}.svg`} alt="" /></p>
+                            {/* 天氣圖名稱 */}
+                            <p>
+                              {time.parameter.parameterName}
+                            </p>
+                            {/* 下雨機率 */}
+                            <p className="pRain"><IoIosUmbrella />10%</p>
+                          </div>
+                        )
+                      })
+                    }
 
 
+                  </div>
+
+
+                </div>
+              )
+            })
+          }
+
+        </div>
       </section>
 
     </>
