dfb2885674e4f1941caf6c3f200eb05207a17e87
Ziniaaa <ziniania0727@gmail.com>
2024-12-16 17:02:06 +0800

    1216

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -17,12 +17,12 @@ export default function App() {
     // 新增html元素 → html()
     // 取得子元素 → children()
 
-    $('a:has(.ttpShow)').mouseover(function (e) {
+    $('a:has(.ttpShow)').on('mouseover',function (e) {
       $('body').append('<div id="ttpPanel">' + $(this).children('.ttpShow').html() + '</div>');
       $('#ttpPanel').css({ top: (e.pageY + 10) + 'px', left: (e.pageX + 10) + 'px' }).fadeIn('fast');
-    }).mouseout(function (e) {
+    }).on('mouseout',function (e) {
       $('#ttpPanel').remove();
-    }).mousemove(function (e) {
+    }).on('mousemove',function (e) {
       $('#ttpPanel').css({ top: (e.pageY + 10) + 'px', left: (e.pageX + 10) + 'px' }).fadeIn('fast');
     })
   }, []);
@@ -36,7 +36,7 @@ export default function App() {
         Lorem ipsum, dolor sit amet
         <a href="#">consectetur<span className="ttpShow">consectetur說明</span></a>
         adipisicing elit. Dolore suscipit delectus iure dignissimos cupiditate quisquam aliquam minima? Ducimus doloremque architecto recusandae alias? Voluptates obcaecati, dolore tempore ipsa quam vitae ab molestias optio quos, dicta sed sit dolorum similique dignissimos itaque vel explicabo deserunt commodi! Magnam deserunt mollitia odit atque? Neque!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore suscipit delectus iure dignissimos cupiditate quisquam aliquam minima? Ducimus doloremque architecto recusandae alias? Voluptates obcaecati, dolore tempore ipsa quam vitae ab molestias optio quos, dicta sed sit dolorum similique dignissimos itaque vel explicabo deserunt commodi! Magnam deserunt <a href="#">mollitia<span className="ttpShow"> <img src="./images/03.jpg" alt="" /> mollitia說明</span></a> odit atque? Neque!
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore suscipit delectus iure dignissimos cupiditate quisquam aliquam minima? Ducimus doloremque architecto recusandae alias? Voluptates obcaecati, dolore tempore ipsa quam vitae ab molestias optio quos, dicta sed sit dolorum similique dignissimos itaque vel explicabo deserunt commodi! Magnam deserunt <a href="#">mollitia<span className="ttpShow"> <img src="./images/03.jpg" alt="" style={{width:'100px'}}/> </span></a> odit atque? Neque!
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo tempora delectus voluptates! Temporibus provident aliquam, facilis, consectetur esse laborum blanditiis natus repudiandae nostrum, eaque iusto delectus magnam officia? Alias eos quod ipsum explicabo eveniet deserunt laboriosam nam pariatur, dicta fugiat. Dolor architecto fugit ab corporis. Quaerat architecto doloremque tenetur laborum sit ipsa, consectetur exercitationem id rem veniam, a in nostrum.
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus perspiciatis dolore explicabo aperiam quos adipisci consequatur soluta reprehenderit nobis repellendus possimus nemo in, corrupti, minima ad accusamus sit. Similique, eos! Autem dolores recusandae sequi! Rerum rem repellendus nobis sit, sunt quae voluptatem praesentium ipsam asperiores ea voluptates blanditiis aperiam placeat et vero consectetur dignissimos magni! Aliquam totam distinctio veniam obcaecati reiciendis expedita quasi? Aliquam itaque repellat molestias nobis quisquam ab assumenda atque, repudiandae, voluptas, numquam consequatur ipsum aspernatur odio laudantium!
       </p>
