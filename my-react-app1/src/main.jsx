0cff0eb3be0f8746bc8796a5abb9d4b18dac7740
Ziniaaa <ziniania0727@gmail.com>
2024-12-25 15:18:30 +0800

    1225

--- my-react-app1/src/main.jsx
+++ my-react-app1/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App1223_slide自動輪播'
+import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 // import './html/assets/all.scss'
 import './App.css'
